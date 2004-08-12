06d4da33be9f059c050a8a587ca83a078ae62439
Kurt Hornik <Kurt.Hornik@R-project.org>
2004-08-12 00:00:00 +0000

    version 0.2-25

---
+++ R/df.inv.R
@@ -0,0 +1,15 @@
+"df.inv" <-
+function(d, df, lambda = 10^4, iterations = 10)
+{
+	df.diriv <- function(d, lambda)
+ - sum((d * lambda)/(d + lambda)^2)
+	current.df <- sum(d/(d + lambda))	
+	#	cat("lambda,df", lambda, current.df, "\n")
+	if(abs((df - current.df)/df) < 0.0001 | iterations == 1)
+		return(lambda, df = current.df)
+	else {
+		lambda <- exp(log(lambda) - (current.df - df)/df.diriv(d, 
+			lambda))
+		Recall(d, df, lambda, iterations - 1)
+	}
+}
